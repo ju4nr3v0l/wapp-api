803e74393f3af8b9d433acaba03bca25857b1935
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2022-06-08 14:31:46 -0500

    add WhatsAppController.php recieve message function

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -4,12 +4,19 @@ namespace App\Http\Controllers;
 
 
 
+use Laravel\Lumen\Http\Request;
+
 class WhatsAppController extends Controller
 {
 
-    public function receiveMessage()
+    public function receiveMessage(Request $request)
     {
-
+        $payload = $request->getContent();
+        $toSave = json_encode($payload);
+        $test = new Test();
+        $test->nombre = $toSave;
+        $test->apellido = 'Request de whatsapp';
+        $test->save();
         return response('Deleted Successfully', 200);
     }
 }
