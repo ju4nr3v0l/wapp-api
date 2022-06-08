60378a6e5f8ae60b0dddc682381bb7c3c0f1558e
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2022-06-08 11:38:49 -0500

    Add webhook

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -7,7 +7,11 @@ use Illuminate\Http\Request;
 
 class AuthorController extends Controller
 {
-
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function showAllTests()
     {
         return response()->json(Test::all());
