8241a0c18f7a221907c709d0939f85ed6fd115c9
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2022-06-08 11:39:24 -0500

    Add webhook

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Test;
 use Illuminate\Http\Request;
 
-class AuthorController extends Controller
+class TestController extends Controller
 {
     /**
      * Display a listing of the resource.
