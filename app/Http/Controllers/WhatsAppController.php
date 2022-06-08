441c0d8807d6d0a812776a8909ef49078364fee7
ju4nr3v0l <juandavidmarulanda@yahoo.com>
2022-06-08 18:02:24 -0500

    add WhatsAppController.php recieve message function

--- app/Http/Controllers/WhatsAppController.php
+++ app/Http/Controllers/WhatsAppController.php
@@ -3,7 +3,6 @@
 namespace App\Http\Controllers;
 
 use App\Test;
-use App\WhatsappMessage;
 use Illuminate\Http\Request;
 
 class WhatsAppController extends Controller
@@ -11,12 +10,12 @@ class WhatsAppController extends Controller
 
     public function receiveMessage(Request $request)
     {
-        $payload = $request->getContent();
+        $payload = $request->get('hub_challenge');
         $toSave = json_encode($payload);
-        $wapMessage = new WhatsappMessage();
-        $wapMessage->content = $toSave;
-        $wapMessage->channel = 'whatsapp';
-        $wapMessage->save();
-        return response('guarde en la nueva tabla', 200);
+        $test = new Test();
+        $test->nombre = $toSave;
+        $test->apellido = 'Request de whatsapp';
+        $test->save();
+        return response($payload, 200);
     }
 }
